b76229bb2e46178b9de8fc1994febd68c4bff030
Anthony D <CypharX@gmail.com>
2020-10-08 00:22:51 -0400

    removed regex

---
+++ Scripts/Solve.js
@@ -0,0 +1,33 @@
+﻿//Setup for the "flipBtn" button to call an anonymous function when clicked
+document.getElementById('flipBtn').addEventListener('click', function () {
+    
+    let inputWord = document.getElementById('reverseThis').value;
+    let lowerWord = inputWord.toLowerCase();
+    let truncInput = lowerWord.split(' ').join('');
+    let flippedWord = ""
+    for (let loop = inputWord.length - 1; loop >= 0; loop--) {
+        flippedWord += truncInput.charAt(loop);
+    }
+    if (truncInput == flippedWord) {
+        swal('Good Job', `${inputWord} is a palindrome!`, 'success',)
+    }
+    else {
+        swal('Sorry', `${inputWord} is not a palindrome!`, 'error')
+    }
+
+
+    document.getElementById('flippedText').innerHTML = flippedWord
+
+
+
+})
+document.getElementById('reverseThis').addEventListener('keypress', function (evt) {
+    var char = evt.which;
+    if (char >= 97 && char <= 122 || char >= 65 && char <= 90 || char == 32) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        return false;
+    }
+});
